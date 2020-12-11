02912c9310b8ba77256a41789eb3125bc9416e7b
Alexander Hildebrandt <github@hillburn.net>
2020-12-11 15:03:55 +0100

    documentation

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 import csv
 
-collection = []
 with open('data/semester_1_clean.csv', newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
 
+    # this hold the full flashcard data.
+    collection = []
+    # this hold the data of one double-sided page.
     chunk = {
         'front_rows': [],
         'back_rows': []
     }
+    # these two hold data for one row of a double-sided page.
     front_cells = []
     back_cells = []
     for row in csv_reader:
+        # data is gathered from the csv.
         front_cells.append(row[0])
         back_cells.append(
             {
@@ -23,6 +27,8 @@ with open('data/semester_1_clean.csv', newline='') as csv_file:
         )
 
         if len(front_cells) == 3:
+            # when we have enough data for one row  of a page, we add it to the chunks.
+            # we have to do it like this, because the back rows have to be reversed to line up with the front side.
             chunk['front_rows'].append(front_cells)
             back_cells.reverse()
             chunk['back_rows'].append(back_cells)
@@ -30,13 +36,14 @@ with open('data/semester_1_clean.csv', newline='') as csv_file:
             back_cells = []
 
         if len(chunk['front_rows']) == 4:
+            # when we have enough data for one double-side page, we add it to the full collection.
             collection.append(chunk)
             chunk = {
                 'front_rows': [],
                 'back_rows': []
             }
 
-    # remaining cells are appended with placeholders, then added to chunk as usual.
+    # the final page might not be filled completely. we have to add placeholders for everything to line up properly.
     cell_count = len(front_cells)
     if cell_count == 1:
         front_cells.append('')
