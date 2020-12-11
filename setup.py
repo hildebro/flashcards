54295915076dfe39bcff8410001199dcccd05199
Alexander Hildebrandt <github@hillburn.net>
2020-12-11 16:57:09 +0100

    rename package in order to get rid of hyphen

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flashcard-builder",
+    name="flashburner",
     version="0.0.1",
     author="Alexander Hildebrandt",
     author_email="github@hillburn.net",
