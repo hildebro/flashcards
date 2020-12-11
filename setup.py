97953911c0f54a7d61fab565b7bdee3bee4f3750
Alexander Hildebrandt <github@hillburn.net>
2020-12-11 16:24:30 +0100

    packaging changes

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flashcards",
+    name="flashcard-builder",
     version="0.0.1",
     author="Alexander Hildebrandt",
-    author_email="alex@hillburn.net",
-    description="Create flashcards on PDFs to print and cut out",
+    author_email="github@hillburn.net",
+    description="Create printable HTML flashcards",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hildebro/flashcards",
+    url="https://github.com/hildebro/python-flashcard-builder",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    include_package_data=True,
+    package_data={'': ['templates/*.html']},
+    python_requires='>=3.9',
 )
